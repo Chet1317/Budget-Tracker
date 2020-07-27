258a7ded6adff743093702b456b5642ac8b7b30a
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-27 13:52:57 -0400

    added mongoose.connections to server.js

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/27017/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
-// routes
+var MONODB_URI = process.env.MONGODB_URI || "mongodb://localhost/27017/mongoHeadlines";
+
+mongoose.connect(MONODB_URI);
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
